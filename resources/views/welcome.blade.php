e4f6308d4d593dc5452c906a1d9bb94b150079f2
ooocrniooo <sinisa.prkic@gmail.com>
2015-09-08 11:01:12 +0200

    Change welcome message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
 	<body>
 		<div class="container">
 			<div class="content">
-				<div class="title">miniCMS for Agency</div>
+				<div class="title">miniCMS for Travel Agency</div>
 				<div class="quote">{{ Inspiring::quote() }}</div>
 			</div>
 		</div>

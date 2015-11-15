69244bc0f884ab7fe8476f2de9fa2009b94940ad
ooocrniooo <sinisa.prkic@gmail.com>
2015-11-15 11:23:07 +0100

    Crni iiii aaaa

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
 				<div class="panel-heading">Home</div>
 
 				<div class="panel-body">
-					You are logged in! CRNI iiiiii
+					You are logged in! CRNI iiiiii aaaaaaa
 				</div>
 			</div>
 		</div>
